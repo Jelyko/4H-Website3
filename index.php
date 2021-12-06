f5d3538b704d354fb8cb49c3ffe1b2a6c49d1727
Jelyko TCG <jasper.demmers@hotmail.com>
2021-12-06 17:06:49 +0100

    Updated #intro h1

--- index.php
+++ index.php
@@ -43,48 +43,48 @@
                     <nav>
                         <ul id="nav">
                             <?php
-                            if(isset($_GET["page"])) {
-                                $nummer = $_GET["page"];
-                            } else {
-                                $nummer = 1;
-                            }
-                            
-                            if ($nummer == 1) {
-                                echo "<li id='nav'><a id='nav' href='index.php?page=1' class='active'>Home</a></li>";
-                            } else {
-                                echo "<li id='nav'><a id='nav' href='index.php?page=1'>Home</a></li>";
-                            }
-                            if ($nummer == 2) {
-                                echo "<li id='nav'><a id='nav' href='index.php?page=2' class='active'>Overmij</a></li>";
-                            } else {
-                                echo "<li id='nav'><a id='nav' href='index.php?page=2'>Overmij</a></li>";
-                            }
-                            if ($nummer == 3) {
-                                echo "<li id='nav'><a id='nav' href='index.php?page=3' class='active'>Opdrachten</a></li>";
-                            } else {
-                                echo "<li id='nav'><a id='nav' href='index.php?page=3'>Opdrachten</a></li>";
-                            }
-                            if ($nummer == 4) {
-                                echo "<li id='nav'><a id='nav' href='index.php?page=4' class='active'>Dagboek</a></li>";
-                            } else {
-                                echo "<li id='nav'><a id='nav' href='index.php?page=4'>Dagboek</a></li>";
-                            }
-                            if ($nummer == 5) {
-                                echo "<li id='nav'><a id='nav' href='index.php?page=5' class='active'>Onderwerp</a></li>";
-                            } else {
-                                echo "<li id='nav'><a id='nav' href='index.php?page=5'>Onderwerp</a></li>";
-                            }
-                            if ($nummer == 6) {
-                                echo "<li id='nav'><a id='nav' href='index.php?page=6' class='active'>Rekenmachine</a></li>";
-                            } else {
-                                echo "<li id='nav'><a id='nav' href='index.php?page=6'>Rekenmachine</a></li>";
-                            }
-                            if ($nummer == 7) {
-                                echo "<li id='nav'><a id='nav' href='index.php?page=7' class='active'>Bronnen</a></li>";
-                            } else {
-                                echo "<li id='nav'><a id='nav' href='index.php?page=7'>Bronnen</a></li>";
-                            }
-                            echo "<li id='nav'><a id='nav' href='website1/index.html'>Website 1</a></li>";
+                                if(isset($_GET["page"])) {
+                                    $nummer = $_GET["page"];
+                                } else {
+                                    $nummer = 1;
+                                }
+                                
+                                if ($nummer == 1) {
+                                    echo "<li id='nav'><a id='nav' href='index.php?page=1' class='active'>Home</a></li>";
+                                } else {
+                                    echo "<li id='nav'><a id='nav' href='index.php?page=1'>Home</a></li>";
+                                }
+                                if ($nummer == 2) {
+                                    echo "<li id='nav'><a id='nav' href='index.php?page=2' class='active'>Overmij</a></li>";
+                                } else {
+                                    echo "<li id='nav'><a id='nav' href='index.php?page=2'>Overmij</a></li>";
+                                }
+                                if ($nummer == 3) {
+                                    echo "<li id='nav'><a id='nav' href='index.php?page=3' class='active'>Opdrachten</a></li>";
+                                } else {
+                                    echo "<li id='nav'><a id='nav' href='index.php?page=3'>Opdrachten</a></li>";
+                                }
+                                if ($nummer == 4) {
+                                    echo "<li id='nav'><a id='nav' href='index.php?page=4' class='active'>Dagboek</a></li>";
+                                } else {
+                                    echo "<li id='nav'><a id='nav' href='index.php?page=4'>Dagboek</a></li>";
+                                }
+                                if ($nummer == 5) {
+                                    echo "<li id='nav'><a id='nav' href='index.php?page=5' class='active'>Onderwerp</a></li>";
+                                } else {
+                                    echo "<li id='nav'><a id='nav' href='index.php?page=5'>Onderwerp</a></li>";
+                                }
+                                if ($nummer == 6) {
+                                    echo "<li id='nav'><a id='nav' href='index.php?page=6' class='active'>Rekenmachine</a></li>";
+                                } else {
+                                    echo "<li id='nav'><a id='nav' href='index.php?page=6'>Rekenmachine</a></li>";
+                                }
+                                if ($nummer == 7) {
+                                    echo "<li id='nav'><a id='nav' href='index.php?page=7' class='active'>Bronnen</a></li>";
+                                } else {
+                                    echo "<li id='nav'><a id='nav' href='index.php?page=7'>Bronnen</a></li>";
+                                }
+                                echo "<li id='nav'><a id='nav' href='website1/index.html'>Website 1</a></li>";
                             ?>
                             <div id="logo">
                                 <img id="logo" src="assets/IMG/logo.jpg" width="40px" height="40px">
@@ -92,7 +92,37 @@
                         </ul>
                     </nav>
                     <div id="intro">
-                        <h1 id="title">Jaspers site</h1>
+                        <?php
+                            if(isset($_GET["page"])) {
+                                $nummer = $_GET["page"];
+                            } else {
+                                $nummer = 1;
+                            }
+                            
+                            echo "<h1 id='title'>";
+                            if ($nummer == 1) {
+                                echo "Jaspers site";
+                            }
+                            if ($nummer == 2) {
+                                echo "Overmij";
+                            }
+                            if ($nummer == 3) {
+                                echo "Opdrachten";
+                            }
+                            if ($nummer == 4) {
+                                echo "Dagboek";
+                            }
+                            if ($nummer == 5) {
+                                echo "Onderwerp";
+                            }
+                            if ($nummer == 6) {
+                                echo "Rekenmachine";
+                            }
+                            if ($nummer == 7) {
+                                echo "Bronnen";
+                            }
+                            echo "</h1>";
+                        ?>
                     </div>
                 </header>
 
